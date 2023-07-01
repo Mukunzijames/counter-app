1ee587877523fed1e7d522219e84fcc3427008f1
unknown <mukunzindahiro@gmail.com>
2023-07-01 16:20:34 -0700

    1st commit

---
+++ script.js
@@ -0,0 +1,32 @@
+// set initial count
+let count = 0 ;
+
+// select value and button
+const value = document.querySelector('#value');
+const button = document.querySelectorAll('button')
+
+button.forEach((butto)=>{
+ butto.addEventListener('click', (e) =>{
+    const style = e.currentTarget.classList ;
+    if (style.contains("decrease")) {
+        count--;
+    }
+    else if (style.contains("increase")) {
+        count++;
+    }
+    else{
+    count = 0;
+   }
+   if (count < 0) {
+    value.style.color = 'red';
+   }
+   else if (count > 0) {
+    value.style.color = 'blue';
+   }
+   else{
+    value.style.color ='black';
+   }
+    value.textContent = count;
+ 
+ })
+})
