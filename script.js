2237e81cdf6c58966ed0982924320281efa0be8f
ndahiro james <mukunzindahiro@gmail.com>
2023-11-08 22:31:43 -0800

    changes

--- script.js
+++ script.js
@@ -38,3 +38,7 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     
 }
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
