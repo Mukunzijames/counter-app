0826719f86a783885a3e2ce861ed8855c75a99c0
ndahiro james <mukunzindahiro@gmail.com>
2023-11-11 05:26:22 -0800

    changes

---
+++ function.js
@@ -0,0 +1,11 @@
+// function declaration
+
+function name() {
+    console.log('mukunzi ndahiro james')
+}
+name()
+// function expression
+const title =function() {
+    console.log('head master')
+}
+title()
---
+++ loop.js
@@ -0,0 +1,19 @@
+// for(let i = 1; i < 10 ;i++){
+// console.log(i)
+// }
+
+const product = ['mango','orange','avocado','primus','dodo','carrott']
+
+// for (let i= 0; i< product.length;i++) {
+
+// console.log('food:',product[i])
+// }
+let i = 0
+while (i < product.length) {
+    console.log("food:",product[i])
+    i++
+}
+
+do {
+    
+} while (condition);
