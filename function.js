1ba84f58e2a2ac57765cfb8a8653220eb69d121d
ndahiro james <mukunzindahiro@gmail.com>
2023-11-12 19:59:49 -0800

    changes

--- function.js
+++ function.js
@@ -9,7 +9,37 @@ const title =function() {
     console.log('head master')
 }
 title()
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
+
+// function hello (name){
+//     console.log(`good morning ${name}`)
+// }
+// hello('james')
+
+const hello =  function(name){
+console.log(`greeting ${name}`)
 }
+hello('james')
+
+// arrow function
+const calcArea = (radius) => 3.14*radius**2;
+
+const area =calcArea(5)
+console.log(area)
+
+const bill = (product,tax)=>{
+
+}
+// call back and foreach
+const myFunc = (callbackFunk) =>{
+    let value = 50
+    callbackFunk(value)
+}
+myFunc(function(value){
+console.log(value)
+})
+
+const materials =['shoes','clothes','hoes','basket','rack']
+
+materials.forEach(person=>{
+console.log("player:",person)
+})
