ea9a5ec41397f8c23a05658a1435761a04980df0
ndahiro james <mukunzindahiro@gmail.com>
2023-11-24 20:57:39 -0800

    changes

--- function.js
+++ function.js
@@ -179,7 +179,3 @@ for (let index = 0; index < array.length; index++) {
     const element = array[index];
     
 }
-for (let index = 0; index < array.length; index++) {
-    const element = array[index];
-    
-}
