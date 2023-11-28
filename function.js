6c3ffb7811b1cd31de8a710d9a2898399b29344e
ndahiro james <mukunzindahiro@gmail.com>
2023-11-27 20:46:00 -0800

    changes

--- function.js
+++ function.js
@@ -172,7 +172,14 @@ let user = {
 
 user.logBlog()
 
-
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+    for (let index = 0; index < array.length; index++) {
+        const element = array[index];
+        
+    }
+}
 for (let index = 0; index < array.length; index++) {
     const element = array[index];
     
